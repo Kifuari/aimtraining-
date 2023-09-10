084085bbfacadc0d37a36cfdcc439926de3c5bbd
Kifuari <dariyszzz@gmail.com>
2023-09-10 16:48:08 +0300

    Update

--- content/config.mjs
+++ content/config.mjs
@@ -7,4 +7,5 @@ const config = {
     // links to the pages you want to link to in the navbar and in the footer
     navLinks: [{ href: "/Games", name: "Games" }],
   };
-export default {config};
+
+export default config;
